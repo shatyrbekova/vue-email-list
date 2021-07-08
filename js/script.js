3d3e8660eac5ea5193c901d1a864e45a6bf5a866
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-07-08 15:47:25 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -4,34 +4,57 @@ new Vue(
     {
         el: '#app',
         data: {
-            listEmails: [],
-        }, //*data si chiude qua
+            listGeneratedEmails:[],
+            listEmails: [], //ogni e-mail che viene generata verrà salvata nell'array listEmails:
+             currentEmail: 0
+        }, //data si chiude qua
 
         created() {
           
-            this.getEmailsList();
+            this.getEmailsList(); //!per stampare le emails devo richiamare la funzione, per richiamare la funzioen 
+                                 //!getEmailsList uso obbligatoriamente "THIS"
+           this.getGeneratedEmailsList();
 
         }, //*created si chiude qua
 
         methods:{
           
             getEmailsList: function(){
-                
+
+                //per generare 10 volte emails, ci serve il ciclo for o while
+
                 for(let x=0; x <10; x++){
                     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                     .then((response) => {  // appena il server ci risponde, si attiva questa funzione
-                        this.listEmails.push(response.data.response);
+                        this.listEmails.push(response.data.response);//ogni e-mail generata verrà pushata nell'array listEmails
                     });
                 };
-                // let x =0;
-                // while(x<10){
-                //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                //     .then((response) => {  // appena il server ci risponde, si attiva questa funzione
-                //     this.listEmails.push(response.data.response);
-                // });
-                // }
+                
+                                  //!Bonus 
+               //*Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+            },
             
+            getGeneratedEmailsList: function(){
+              
+                let emailCounted = 0;
+                while (emailCounted <10 ){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {  // appena il server ci risponde, si attiva questa funzione
+                        this.listGeneratedEmails.push(response.data.response);//ogni e-mail generata verrà pushata nell'array listEmails
+                    });
+                    emailCounted++;
+              }
+
+               
+                
+               
+
             },
 
-         },//*methods si chiude qua
+
+
+
+
+         },//methods si chiude qua
     });
